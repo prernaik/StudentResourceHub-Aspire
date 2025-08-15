d4a6b1937b5ac386885cebc74a3f22383f97692a
prernaik <prerna20co15@gmail.com>
2025-08-15 20:50:45 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/home.js";
+import Upload from "c:/Users/prerna naik/Documents/GitHub/StudentResourceHub-Aspire/frontend/src/pages/upload.js";
+import Notes from "c:/Users/prerna naik/Documents/GitHub/StudentResourceHub-Aspire/frontend/src/pages/notes.js";
+import About from "c:/Users/prerna naik/Documents/GitHub/StudentResourceHub-Aspire/frontend/src/pages/about.js";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+       <h1>Student Resource Hub</h1>
+      </div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/upload">Upload</Link>
+        <Link to="/notes">Notes</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Home() {
+  return <h2>Welcome to Student Resource Hub</h2>;
+}
+
+export default Home;
